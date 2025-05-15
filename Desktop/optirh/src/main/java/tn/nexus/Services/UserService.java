44163c1b2fa113f8aaeea9ba843d876d8b69d80a
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2025-05-15 16:15:37 +0100

    fix "password column"

--- Desktop/optirh/src/main/java/tn/nexus/Services/UserService.java
+++ Desktop/optirh/src/main/java/tn/nexus/Services/UserService.java
@@ -19,7 +19,7 @@ public class UserService implements CRUD<User> {
     @Override
     public int insert(User user) throws SQLException {
 
-        String req = "INSERT INTO Users(nom, email, motDePasse, role, address) VALUES (?, ?, ?, ?, ?)";
+        String req = "INSERT INTO User(nom, email, mot_de_passe, role, address) VALUES (?, ?, ?, ?, ?)";
 
         ps = cnx.prepareStatement(req);
 
@@ -34,7 +34,7 @@ public class UserService implements CRUD<User> {
 
     @Override
     public int update(User person) throws SQLException {
-        String req = "UPDATE Users SET nom = ?, email = ?, motDePasse = ?, role = ?, address = ? WHERE id = ?";
+        String req = "UPDATE User SET nom = ?, email = ?, mot_de_passe = ?, role = ?, address = ? WHERE id = ?";
 
         ps = cnx.prepareStatement(req);
 
@@ -52,7 +52,7 @@ public class UserService implements CRUD<User> {
 
     @Override
     public int delete(User person) throws SQLException {
-        String req = "DELETE FROM Users WHERE id = ?";
+        String req = "DELETE FROM User WHERE id = ?";
 
         ps = cnx.prepareStatement(req);
 
@@ -65,7 +65,7 @@ public class UserService implements CRUD<User> {
     public List<User> showAll() throws SQLException {
         List<User> temp = new ArrayList<>();
 
-        String req = "SELECT * FROM `users`";
+        String req = "SELECT * FROM `user`";
 
         st = cnx.createStatement();
 
@@ -86,7 +86,7 @@ public class UserService implements CRUD<User> {
         return temp;
     }
     public User getUserByEmailAndRole(String email, Role role) throws SQLException {
-        String req = "SELECT * FROM Users WHERE email = ? AND role = ?";
+        String req = "SELECT * FROM User WHERE email = ? AND role = ?";
         try (PreparedStatement ps = cnx.prepareStatement(req)) {
             ps.setString(1, email);
             ps.setString(2, role.name()); // Convertir le rôle en chaîne de caractères
@@ -105,7 +105,7 @@ public class UserService implements CRUD<User> {
         return null; // Aucun utilisateur trouvé avec cet email et ce rôle
     }
     public User getUserByEmail(String email) throws SQLException {
-        String req = "SELECT * FROM users WHERE email = ?";
+        String req = "SELECT * FROM user WHERE email = ?";
         try (PreparedStatement ps = cnx.prepareStatement(req)) {
             ps.setString(1, email);
             try (ResultSet rs = ps.executeQuery()) {
@@ -114,7 +114,7 @@ public class UserService implements CRUD<User> {
                     user.setId(rs.getInt("id"));
                     user.setNom(rs.getString("nom"));
                     user.setEmail(rs.getString("email"));
-                    user.setPassword(rs.getString("motDePasse"));
+                    user.setPassword(rs.getString("mot_de_passe"));
                     user.setRole(Role.valueOf(rs.getString("role")));
                     user.setAddress(rs.getString("address"));
                     return user;
@@ -124,7 +124,7 @@ public class UserService implements CRUD<User> {
         return null; // Aucun utilisateur trouvé avec cet email
     }
     public String getUserNameById(int userId) throws SQLException {
-        String query = "SELECT nom FROM users WHERE id = ?";
+        String query = "SELECT nom FROM user WHERE id = ?";
 
         try (PreparedStatement statement = cnx.prepareStatement(query)) {
             statement.setInt(1, userId);
@@ -139,7 +139,7 @@ public class UserService implements CRUD<User> {
 
     public User getUserById(int userId) throws SQLException {
         System.out.println("Recherche de l'utilisateur avec l'ID : " + userId); // Log pour débogage
-        String query = "SELECT * FROM users WHERE id = ?";
+        String query = "SELECT * FROM user WHERE id = ?";
         try (PreparedStatement ps = cnx.prepareStatement(query)) {
             ps.setInt(1, userId);
             try (ResultSet rs = ps.executeQuery()) {
@@ -162,7 +162,7 @@ public class UserService implements CRUD<User> {
 
 
     public User getUserById2(int userId) throws SQLException {
-        String query = "SELECT * FROM users WHERE id = ?";
+        String query = "SELECT * FROM user WHERE id = ?";
         try (PreparedStatement ps = cnx.prepareStatement(query)) {
             ps.setInt(1, userId);
             try (ResultSet rs = ps.executeQuery()) {
@@ -171,7 +171,7 @@ public class UserService implements CRUD<User> {
                             rs.getInt("id"),
                             rs.getString("nom"),
                             rs.getString("email"),
-                            rs.getString("motDePasse"),
+                            rs.getString("mot_de_passe"),
                             Role.valueOf(rs.getString("role")),
                             rs.getString("address")
                     );
